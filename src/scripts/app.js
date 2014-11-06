b9d53429c5820c94db743426620d6ad7bac2bb13
Saul <saul.999david@hotmail.com>
2014-11-06 00:13:28 -0800

    sa kdjsa

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -10,20 +10,5 @@ riegoApp.config(function($stateProvider,$urlRouterProvider) {
     url: "/Inicio",
     templateUrl: 'views/Home.html',
     controller: 'HomeCtrl'
-  })
-  .state("control", {
-    url: "/Control",
-    templateUrl: 'views/Control.html',
-    controller: 'ControlCtrl'
-  })
-  .state("mapa", {
-    url: "/Mapa",
-    templateUrl: 'views/Mapa.html',
-    controller: 'MapaCtrl'
-  })
-  .state("estadisticas", {
-    url: "/Estadisticas",
-    templateUrl: 'views/Estadisticas.html',
-    controller: 'EstadisticasCtrl'
   });
 });
--- src/scripts/controllers.js
+++ src/scripts/controllers.js
@@ -7,15 +7,3 @@ riegoControllers.controller("IndexCtrl", function ($rootScope,$scope,$state,$loc
 riegoControllers.controller("HomeCtrl", function ($rootScope,$scope,$location,$state) {
 
 });
-
-riegoControllers.controller("ControlCtrl", function ($rootScope,$scope,$location,$state) {
-  
-});
-
-riegoControllers.controller("MapaCtrl", function ($rootScope,$scope,$location,$state) {
-  
-});
-
-riegoControllers.controller("EstadisticasCtrl", function ($rootScope,$scope,$location,$state) {
-  
-});
