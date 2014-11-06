020ba92a61b41026e2691e8db9623b3691b08b47
Saul <saul.999david@hotmail.com>
2014-11-06 00:13:28 -0800

    sa

--- src/scripts/controllers.js
+++ src/scripts/controllers.js
@@ -9,13 +9,19 @@ riegoControllers.controller("HomeCtrl", function ($rootScope,$scope,$location,$s
 });
 
 riegoControllers.controller("ControlCtrl", function ($rootScope,$scope,$location,$state) {
-  
+	document.getElementById("control").className += " menuActivo";
+	document.getElementById("mapa").className = "";
+	document.getElementById("estadis").className = "";
 });
 
 riegoControllers.controller("MapaCtrl", function ($rootScope,$scope,$location,$state) {
-  
+	document.getElementById("control").className = "";
+	document.getElementById("mapa").className += " menuActivo";
+	document.getElementById("estadis").className = "";
 });
 
 riegoControllers.controller("EstadisticasCtrl", function ($rootScope,$scope,$location,$state) {
-  
+	document.getElementById("control").className = "";
+	document.getElementById("mapa").className = "";
+	document.getElementById("estadis").className += " menuActivo";
 });
